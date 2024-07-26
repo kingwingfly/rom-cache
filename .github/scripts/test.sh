d3b4d3921875f49e92b8006a50dfadccc79fd91e
Louis <836250617@qq.com>
2024-07-26 18:15:05 +0800

    init repo

---
+++ .github/scripts/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+export TERM=xterm-256color
+
+# Statements waiting to be executed
+statements=(
+    "cargo clippy --no-default-features -- -D warnings"
+
+    "cargo test --no-default-features"
+
+#     "LOOM_LOG=trace \
+# LOOM_LOCATION=1 \
+# LOOM_CHECKPOINT_INTERVAL=1 \
+# LOOM_CHECKPOINT_FILE=loom.json \
+# RUSTFLAGS=\"--cfg loom\" \
+# cargo test --no-default-features --features derive --test concurrent_test --release"
+
+    "cargo doc --no-deps --no-default-features"
+)
+
+# loop echo and executing statements
+for statement in "${statements[@]}"; do
+    echo "$(tput setaf 3)$statement$(tput sgr0)"
+    eval $statement
+    echo
+done
