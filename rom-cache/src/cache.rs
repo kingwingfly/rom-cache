dcd088811eebbc532c291c0e8710028a6161677b
Louis <836250617@qq.com>
2024-07-30 01:02:40 +0800

    fix: doc mistake

--- rom-cache/src/cache.rs
+++ rom-cache/src/cache.rs
@@ -302,7 +302,7 @@ pub trait Cacheable: Any + Send + Sync {
     fn as_any(&self) -> &dyn Any;
     /// As Any mut.
     /// ```ignore
-    /// fn as_any_mut(&self) -> &mut dyn Any {
+    /// fn as_any_mut(&mut self) -> &mut dyn Any {
     ///     self
     /// }
     /// ```
